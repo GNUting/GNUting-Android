00636561c52ad6a4ece60a796f3eb3ee77337624
changs97 <kin2027@gmail.com>
2024-01-20 18:21:48 +0900

    first commit

---
+++ app/src/main/java/com/changs/android/gnuting_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.changs.android.gnuting_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
