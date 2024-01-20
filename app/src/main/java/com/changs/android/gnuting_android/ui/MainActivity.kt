f03925e8915d00ae89e1545bf49219828d6aeeb5
changs97 <kin2027@gmail.com>
2024-01-20 20:51:06 +0900

    Remove: 상태바 변경 코드 제거

--- app/src/main/java/com/changs/android/gnuting_android/ui/MainActivity.kt
+++ app/src/main/java/com/changs/android/gnuting_android/ui/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import com.changs.android.gnuting_android.R
 import com.changs.android.gnuting_android.databinding.ActivityMainBinding
-import com.changs.android.gnuting_android.util.setFullScreenWithStatusBar
-import com.changs.android.gnuting_android.util.setStatusBarColor
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -15,19 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setFullScreenWithStatusBar(this)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         val navController = navHostFragment.navController
-
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.startFragment -> setStatusBarColor(R.color.transparent)
-                else -> setStatusBarColor(R.color.white)
-            }
-        }
     }
 }
--- app/src/main/java/com/changs/android/gnuting_android/util/Utils.kt
+++ app/src/main/java/com/changs/android/gnuting_android/util/Utils.kt
@@ -6,34 +6,3 @@ import android.os.Build
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-
-@Suppress("DEPRECATION")
-fun setFullScreenWithStatusBar(activity: AppCompatActivity) {
-    activity.apply {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-    }
-}
-
-@Suppress("DEPRECATION")
-fun Activity.setStatusBarColor(color: Int) {
-    var flags = window?.decorView?.systemUiVisibility // get current flag
-    if (flags != null) {
-        if (isColorDark(color)) {
-            flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-            window?.decorView?.systemUiVisibility = flags
-        } else {
-            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            window?.decorView?.systemUiVisibility = flags
-        }
-    }
-    window?.statusBarColor = color
-}
-
-private fun isColorDark(color: Int): Boolean {
-    val darkness =
-        1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
-    return darkness >= 0.5
-}
