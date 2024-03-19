1ce2c9a009258f915e75e05b872817dd2aaa103f
changs97 <kin2027@gmail.com>
2024-03-20 02:13:04 +0900

    :lipstick: 바텀 시트 UI 수정

--- app/src/main/java/com/changs/android/gnuting_android/ui/fragment/CurrentMemberBottomSheetFragment.kt
+++ app/src/main/java/com/changs/android/gnuting_android/ui/fragment/CurrentMemberBottomSheetFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.changs.android.gnuting_android.data.model.InUser
@@ -21,12 +22,28 @@ class CurrentMemberBottomSheetFragment(private val currentMember: List<InUser>) 
     private val binding get() = _binding!!
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val bottomSheetDialog = super.onCreateDialog(savedInstanceState)
-        if (bottomSheetDialog is BottomSheetDialog) {
+        val dialog = BottomSheetDialog(requireContext(), theme)
+        dialog.setOnShowListener {
+
+            val bottomSheetDialog = it as BottomSheetDialog
             bottomSheetDialog.behavior.skipCollapsed = true
-            bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+            val parentLayout =
+                bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+
+            parentLayout?.let {
+                val behaviour = BottomSheetBehavior.from(it)
+                setupFullHeight(it)
+                behaviour.state = BottomSheetBehavior.STATE_EXPANDED
+            }
         }
-        return bottomSheetDialog
+        return dialog
+    }
+
+    private fun setupFullHeight(bottomSheet: View) {
+        val layoutParams = bottomSheet.layoutParams
+        layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+        bottomSheet.layoutParams = layoutParams
     }
 
     override fun onCreateView(
